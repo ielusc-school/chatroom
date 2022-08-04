02909c45c24080dcb5f7d7ebb122bf07f7e271ba
Cris IELUSC <cristofer.sousa@ielusc.br>
2022-08-03 22:13:22 -0300

    feat: added socket

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 const app = require('./config/server');
 
-app.listen(5000, () => {
- console.log('app start => http://locahost:5000');
+const server = app.listen(5000, () => {
+  console.log('app start => http://locahost:5000');
+ });
+
+const io = require('socket.io')(server);
+
+// setando a variavel de socket io para que todo a minha app possa utilizar
+app.set('io', io);
+
+io.on('connection', (socket) => {
+  console.log('ENTREI NA SALA');
+
+  socket.on('disconnect', () => {
+    console.log('SAIU DA SALA');
+  });
+
+  socket.on('msgParaServidor', (data) => {
+    socket.emit('msgParaCliente', {
+      apelido: data.apelido,
+      mensagem: data.mensagem,
+    });  // erro estava aqui 
+
+    socket.broadcast.emit('msgParaCliente', {
+      apelido: data.apelido,
+      mensagem: data.mensagem,
+    });
+  });
 });
+
+
+
+
